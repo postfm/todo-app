3078ce5f13eaafadf498b111b2ca7a104386c312
postfm <postfm@yandex.ru>
2021-06-21 22:44:59 +0300

    fix searchpanel

--- src/components/search-panel/SearchPanel.js
+++ src/components/search-panel/SearchPanel.js
@@ -1,26 +1,9 @@
-import React from "react";
 import "./SeachPanel.scss";
 
-class SearchPanel extends React.Component {
-  state = { term: "" };
-
-  onSearchChange = (e) => {
-    const term = e.target.value;
-    this.setState({ term });
-    this.props.onSearchChange(term);
+function SearchPanel({ onSearchChange }) {
+  const getSearchChange = (e) => {
+    onSearchChange(e.target.value);
   };
-  render() {
-    return (
-      <input
-        type="text"
-        className="form-control search-input"
-        placeholder="type here to search"
-        onChange={this.onSearchChange}
-      />
-    );
-  }
-<<<<<<< HEAD
-
   return (
     <input
       type="text"
@@ -29,8 +12,6 @@ class SearchPanel extends React.Component {
       onChange={getSearchChange}
     />
   );
-=======
->>>>>>> parent of bf2a108 (made functional components)
 }
 
 export default SearchPanel;
